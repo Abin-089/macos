19c40984fc439d5da8a252031de8d29857619d1f
ABIN BENNY <133189244+Abin-089@users.noreply.github.com>
2024-05-16 01:07:17 +0530

    Update terminal.tsx

--- src/configs/terminal.tsx
+++ src/configs/terminal.tsx
@@ -13,8 +13,8 @@ const terminal: TerminalData[] = [
         content: (
           <div className="py-1">
             <div>
-              Hi, this is Xiaohan Zou. I am a PhD student at the Computer Science and
-              Engineering department of Pennsylvania State University.
+              Hi, this is Abin Benny. I am a student at the Computer Science and
+              Engineering department.
             </div>
           </div>
         )
@@ -30,7 +30,7 @@ const terminal: TerminalData[] = [
         title: "who-cares.txt",
         type: "file",
         content:
-          "I'm looking for a research internship for Summer 2024. I'm open to collaboration on research projects."
+          "I'm looking for a research internship for Summer. I'm open to collaboration on  projects."
       },
       {
         id: "about-contact",
@@ -42,65 +42,65 @@ const terminal: TerminalData[] = [
               Email:{" "}
               <a
                 className="text-blue-300"
-                href="mailto:renovamenzxh@gmail.com"
+                href="mailto:abinbenny173@gmail.com"
                 target="_blank"
                 rel="noreferrer"
               >
-                renovamenzxh@gmail.com
+                abinbenny173@gmail.com
               </a>
             </li>
             <li>
               Github:{" "}
               <a
                 className="text-blue-300"
-                href="https://github.com/Renovamen"
+                href="https://github.com/Abin-089"
                 target="_blank"
                 rel="noreferrer"
               >
-                @Renovamen
+                @abinbenny
               </a>
             </li>
             <li>
               <a
                 className="text-blue-300"
-                href="https://scholar.google.com/citations?user=RuW6xgMAAAAJ"
+                href="https://animated-portfolio-topaz.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
-                Google Scholar
+                Animated
               </a>
             </li>
             <li>
               Linkedin:{" "}
               <a
                 className="text-blue-300"
-                href="https://www.linkedin.com/in/xiaohan-zou"
+                href="https://www.linkedin.com/in/abin-benny-372365255/"
                 target="_blank"
                 rel="noreferrer"
               >
-                xiaohan-zou
+                Abin Benny
               </a>
             </li>
             <li>
               Personal Website:{" "}
               <a
                 className="text-blue-300"
-                href="https://zxh.me"
+                href="https://abin-089.github.io/personal-blogweb/"
                 target="_blank"
                 rel="noreferrer"
               >
-                https://zxh.me
+                
               </a>
             </li>
             <li>
-              知乎:{" "}
+              :{" "}
               <a
                 className="text-blue-300"
-                href="https://www.zhihu.com/people/chao-neng-gui-su"
+                href="https://animated-portfolio-topaz.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
-                @西伯利亚大恶龙
+                
               </a>
             </li>
           </ul>
